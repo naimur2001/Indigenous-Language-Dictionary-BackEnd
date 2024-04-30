8a6f90fd1e830d9cf6d027103b16478286e28f4b
naimur <naimurrahman18thapril@gmail.com>
2024-05-01 01:47:43 +0600

    cors added

--- src/main/java/com/example/IndigenousLanguageDictionary/RestControllerILD.java
+++ src/main/java/com/example/IndigenousLanguageDictionary/RestControllerILD.java
@@ -1,15 +1,13 @@
 package com.example.IndigenousLanguageDictionary;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin (origins = "*")
 public class RestControllerILD {
  @Autowired
  ServicesILD servicesILD;
